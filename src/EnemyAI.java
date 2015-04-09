d6bd52cbfc380460f5ea7b5247485c07bf14c2ed
EyesAlive <lawlcatsandtequilla@gmail.com>
2015-04-09 14:35:01 -0500

    Ignore the AI please

--- src/EnemyAI.java
+++ src/EnemyAI.java
@@ -6,7 +6,7 @@ import java.security.SecureRandom;
 //Hard is mild cRNG with some economic mumbojumbo
 //European-Extreme is a cheater :) http://docs.oracle.com/javase/tutorial/java/javaOO/accesscontrol.html
 
-//Feel free to ignore this, i'll implement it myself -Kaeyan
+//Feel free to ignore this, i'll implement it myself if I have the time -Kaeyan
 
 public abstract class EnemyAI
 {
