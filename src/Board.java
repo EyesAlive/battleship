da78eed64a550673a7b3b514d47de895b1b5aca2
EyesAlive <lawlcatsandtequilla@gmail.com>
2015-04-09 21:04:27 -0500

    Basic implementation

--- src/Board.java
+++ src/Board.java
@@ -1,12 +1,20 @@
-
+import java.util.ArrayList;
 
 public class Board implements Observer
 {
-	//private coordinates[][]
-	//private size
-	//private <list> ship_list
-	//private <list> sunk_ship_list
+	private Coordinate coordinates[][];
+	private int size;
+	private ArrayList<Ship> ship_list = new ArrayList<>();
+	private ArrayList<Ship> sunk_ship_list = new ArrayList<>();
 	
-	//public update()
-	//public isValidLocation()
+	//Methods - abstract
+	public void update() {}
+	
+	//Methods
+	public boolean isValidLocation(int x, int y)
+	{
+		if ((x > size) || (y > size))
+			return false;
+		return true;
+	}
 }
