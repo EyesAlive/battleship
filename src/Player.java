d7c50b9e4ac66c0075a9fc058b7a0c81499196ad
sepideh <srogha2@uic.edu>
2015-04-12 21:23:52 -0500

    usermove

--- src/Player.java
+++ src/Player.java
@@ -1,10 +1,16 @@
 
 
-public class Player implements Subject
+public abstract class Player implements Subject
 {
 	//protected is_turn
 	//protected move_strategy
 	//protected ship_strategy
+	
+	protected MoveStrategy move_strategy; //private?
+	
+	public void makeMove(){
+		move_strategy.move();
+	}
 	
 	//public makeMove()
 	//public getState()
--- src/User.java
+++ src/User.java
@@ -6,4 +6,5 @@ public class User extends Player
 	
 	//public endGame()
 	//public placeShips()
+	
 }
