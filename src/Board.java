71e78433c7e590f23c4c9ea43004a9f0b12a7312
EyesAlive <lawlcatsandtequilla@gmail.com>
2015-04-16 14:27:43 -0500

    Updated Board Moved around methods, updated comments Fixed isValidLocation() Implemented update() see comment! Fixed constructor, sunk_ship_list is fixed to size num_ships

--- src/Board.java
+++ src/Board.java
@@ -2,42 +2,51 @@ import java.util.ArrayList;
 
 public class Board implements Observer
 {
+	//Fields
 	private Coordinate coordinates[][];
 	private int size;
 	private MoveStrategy move;
 	private ArrayList<Ship> ship_list;
 	private ArrayList<Ship> sunk_ship_list;
 	
-	//Methods - abstract
-	public void update(MoveStrategy newMove) {
-		move = newMove;
-	
-		if(isValidLocation(move.x(),move.y())){
-			
-			
-		}
-		
-		else{
-			
-			
-			
-		}
-		
-		
-	}
-	
-	// constructor
+	//Constructor
 	public Board(int sz, int num_ships) {
 		size = sz;
 		ship_list = new ArrayList<>(num_ships);
-		sunk_ship_list = new ArrayList<>();
+		sunk_ship_list = new ArrayList<>(num_ships);
 	}
 
 	//Methods
 	public boolean isValidLocation(int x, int y)
 	{
-		if ((x > size) || (y > size))
+		if ((x > size) || (y > size) || (x < 1) || (y < 1))
 			return false;
 		return true;
+	}
+	
+	//Methods
+	public void update(MoveStrategy newMove)
+	{
+		move = newMove;
+
+		if(isValidLocation(move.x(),move.y()))
+		{
+			switch(coordinates[move.x()][move.y()].getState()) //wayyyyy too much nesting!
+			{
+			case EMPTY: coordinates[move.x()][move.y()].setState(CoordState.MISS);
+				break;
+			case SHIP: coordinates[move.x()][move.y()].setState(CoordState.HIT);
+				//Find ship in list and set hit / implement sunk?
+				break;
+			default:
+				break;
+			}	
+		}
+		else
+		{
+			//Communicate failure? Throw exception? Lose turn?
+		}
+
+
 	}
 }
