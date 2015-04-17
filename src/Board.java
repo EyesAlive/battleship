a45dc90092bc6ffeda1bbd0183dca8974885e78d
Ryan Jones <ryan.nolan.jones.53@gmail.com>
2015-04-17 12:02:12 -0500

    Work in progress
    Worked on main and added methods to start setting up the game. Made
    changes to Board so that it can register to player.

--- src/Board.java
+++ src/Board.java
@@ -8,57 +8,23 @@ public class Board implements Observer
 	private MoveStrategy move;
 	private ArrayList<Ship> ship_list;
 	private ArrayList<Ship> sunk_ship_list;
-	//private Ship ship;
-	
-<<<<<<< HEAD
-	//Methods - abstract
-	public void update(MoveStrategy newMove) {
-		move = newMove;
-	
-		if(isValidLocation(move.x(),move.y())){
-			
-			if(coordinates[move.x()][move.y()].getState()==CoordState.SHIP){
-				coordinates[move.x()][move.y()].setState(CoordState.HIT);
-				for(Ship ship : ship_list){
-					
-					
-					
-					
-				}
-				
-					
-					
-				
-				
-				
-			}
-			
-			else
-				coordinates[move.x()][move.y()].setState(CoordState.MISS);
-			
-		
-		}
-			
-		
-		else{
-			
-			
-			
-		}
-		
-		
-	}
-	
-	// constructor
-	public Board(int sz, int num_ships) {
-=======
+	private int boardNum; 
+	private Subject player;
 	//Constructor
-	public Board(int sz, int num_ships)
+	public Board(int sz, int num_ships,int newBoardNum,Subject player)
 	{
->>>>>>> origin/master
+		this.player = player;
+		player.add(this);
+		
 		size = sz;
 		ship_list = new ArrayList<>(num_ships);
 		sunk_ship_list = new ArrayList<>(num_ships);
+		boardNum = newBoardNum;
+	}
+	
+	public void register(Subject player){
+		
+		
 	}
 
 	//Methods
--- src/main.java
+++ src/main.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 //Ryan Jones
 //Kristine Lee
 //Kaeyan Jones
@@ -9,12 +12,126 @@
 public class main
 {
 	
+	protected static ArrayList<Player> players;
+	protected static Player            player;
+	protected static Scanner           input;
+	protected static String            userInput;
+	protected static Board             board1;
+	protected static Board             board2;
+	
+	private static void menu(int menuType){
+		
+		if(menuType==0){
+			System.out.println("-----------BattleShip--------------");
+			System.out.println("-----------Main menu--------");
+			System.out.println("S - Start Game");
+			System.out.println("Q - quit");
+		}
+		
+		else if(menuType==1){
+			System.out.println("-------Board Type-------------");
+			System.out.println("L - Large Board ");
+			System.out.println("S - Small Board");
+			System.out.println("C - Custom Board");
+		}
+	}
+	
+	
+	
+	
+	private static void boardSetup(){
+		
+		
+		
+	}
+	
+	
+	private static void setupGame(){
+		//creating arrayList of players
+		players = new ArrayList<Player>();
+		
+		//player 1
+		player  = new Player(0);
+		players.add(player);
+		
+		//player 2
+		player  = new Player(1);
+		players.add(player);
+		
+		//variable to check if done
+		Boolean done = false;
+		
+		menu(1);
+		while(!done){
+			System.out.println("Input: ");
+			userInput = input.nextLine();
+			userInput = userInput.toLowerCase();
+			
+			//Creates a large board
+			if(userInput == " l"){
+				LargeBoardFactory largeBoard = new LargeBoardFactory();
+				board1 = new Board(largeBoard.size,largeBoard.num_ships,0,players.get(0));
+				board2 = new Board(largeBoard.size,largeBoard.num_ships,1,players.get(1));
+				board1.register(players.get(1));
+				board2.register(players.get(0));
+				done  = true;
+			
+			}
+			//Creates a small board
+			else if(userInput == "s"){
+				SmallBoardFactory smallBoard = new SmallBoardFactory();
+				board1 = new Board(smallBoard.size,smallBoard.num_ships,0,players.get(0));
+				board2 = new Board(smallBoard.size,smallBoard.num_ships,1,players.get(1));
+				board1.register(players.get(1));
+				board2.register(players.get(0));
+				done = true;
+				
+			}
+			
+			//Create a Custom board
+			else if(userInput == "c"){
+				CustomBoardFactory customBoard = new CustomBoardFactory();
+				board1 = new Board(customBoard.size,customBoard.num_ships,0,players.get(0));
+				board2 = new Board(customBoard.size,customBoard.num_ships,1,players.get(1));
+				board1.register(players.get(1));
+				board2.register(players.get(0));
+				done = true;
+				
+			}
+			//what was entered was not valid 
+			else
+				System.out.println("Error: NOT VALID INPUT");
+			
+		}
+		
+		
+		
+	}
+	
 	
 	public static void main(String[] args)
 	{
+		
+		input = new Scanner(System.in);
+		menu(0);
 		boolean quit = false;
 		while (!quit)
 		{
+			System.out.print("Input: ");
+			userInput = input.nextLine();
+			userInput = userInput.toLowerCase();
+			
+			if(userInput == "s"){
+				
+				
+				
+			}
+			else if(userInput == "q"){
+				quit = true;
+				
+			}
+			
+			
 			
 		}
 	}
