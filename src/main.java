c0d8f9cce0464c453b5cb3a35f87249be4aa05cb
ryan jones <ryan.nolan.jones.53@gmail.com>
2015-04-21 17:15:35 -0500

    Revert "moved boardFactory.createBoard outside of for loop"
    This reverts commit 071b7b989c67936b60f91d6d42744441785de0a5.

--- src/main.java
+++ src/main.java
@@ -93,7 +93,6 @@ public class main
 	{
 		players = new Player[2];
 		boards = new Board[2];
-		Board setupBoard;
 		players[0] = new User();
 		if (gameType == 1) players[1] = new User();
 		boolean done = false;
@@ -127,11 +126,9 @@ public class main
 		}
 		if (gameType == 0) players[1] = new Computer(boardFactory.size);
 		// create the board and register player to their own board
-		
-		setupBoard = boardFactory.createBoard();
 		for (int i = 0; i < 2; ++i)
 		{
-			boards[i] = setupBoard;
+			boards[i] = boardFactory.createBoard();
 			boards[i].register(players[i]);
 			players[i].numShips(boardFactory.num_ships);
 		}
